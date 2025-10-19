71ca713787748bafebc547903b21eceddcf07eae
dannyyuan <dannyyuan06@gmail.com>
2025-10-19 12:15:09 +0100

    [dy324] feat: githubio pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dannyyuan/', // <-- important for GitHub Pages
+  base: '/',            // root because this is a user site
   plugins: [react()],
+  build: {
+    outDir: 'docs'      // output files into docs/ so we can serve from main/docs
+  }
 })
