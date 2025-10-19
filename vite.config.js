1eeddc813fe2cbb6ad1d8ba9759f840ba3ce594c
dannyyuan <dannyyuan06@gmail.com>
2025-10-19 12:07:50 +0100

    [dy324] fix: config swc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/dannyyuan/', // <-- important for GitHub Pages
-  plugins: [react()]
+  plugins: [react()],
 })
